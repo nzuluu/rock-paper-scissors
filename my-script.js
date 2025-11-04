e0435d86df09aee71fe5512a6e4b875cdf0fbf92
nzuluu <nisayblu2@gmail.com>
2025-11-03 22:11:31 -0800

    created new function named getComputerChoice

--- my-script.js
+++ my-script.js
@@ -1,1 +1,20 @@
-console.log("Hello World")
+//This functions randomly returns “rock”, “paper” or “scissors”
+function getComputerChoice() {
+
+    let computerChoice;
+
+    //This set the value from Math.random method to randomNum
+    //// Expected output: 0, 1 or 2
+    let randomNum = Math.floor(Math.random() * 3);
+
+    if (randomNum == 0 ) {
+        computerChoice = "rock";
+    }
+    else if (randomNum == 1) {
+        computerChoice = "paper";
+    }
+    else {
+        computerChoice = "scissors";
+    }
+    return computerChoice;
+}
